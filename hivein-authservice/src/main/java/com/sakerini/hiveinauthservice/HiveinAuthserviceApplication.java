949117d88186afc3849427bdf394467c6992049a
Sakerini <sakerini1999@gmail.com>
2021-02-23 12:07:07 +0300

    Initial commit for authentication service

---
+++ hivein-authservice/src/main/java/com/sakerini/hiveinauthservice/HiveinAuthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sakerini.hiveinauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveinAuthserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiveinAuthserviceApplication.class, args);
+    }
+
+}
