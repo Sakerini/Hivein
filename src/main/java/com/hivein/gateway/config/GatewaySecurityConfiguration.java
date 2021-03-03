12e25ee4a942f92d5597c29d4893b17f2ad6dc4c
Sakerini <sakerini1999@gmail.com>
2021-03-03 18:58:41 +0300

    :((

--- src/main/java/com/hivein/gateway/config/GatewaySecurityConfiguration.java
+++ src/main/java/com/hivein/gateway/config/GatewaySecurityConfiguration.java
@@ -3,13 +3,13 @@ package com.hivein.gateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableResourceServer
-public class GatewaySecurityConfiguration extends ResourceServerConfigurerAdapter {
+@EnableWebSecurity
+public class GatewaySecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
