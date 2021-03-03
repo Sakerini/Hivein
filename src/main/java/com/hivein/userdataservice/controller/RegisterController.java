c1266391b808fe3f72dd8c303debee2d4dab8c63
Sakerini <sakerini1999@gmail.com>
2021-03-03 17:28:37 +0300

    Fixed mapping bug

--- src/main/java/com/hivein/userdataservice/controller/RegisterController.java
+++ src/main/java/com/hivein/userdataservice/controller/RegisterController.java
@@ -13,10 +13,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/register")
+@RestController
+@RequestMapping("/register")
 public class RegisterController {
 
     private final RegisterService registerService;
